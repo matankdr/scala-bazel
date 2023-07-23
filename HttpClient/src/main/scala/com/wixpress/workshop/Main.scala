238273ca4141a36540b773939dd6b9169f22f615
Matan Keidar <matank@wix.com>
2023-07-23 18:30:57 +0300

    Initial commit

--- HttpClient/src/main/scala/com/wixpress/workshop/Main.scala
+++ HttpClient/src/main/scala/com/wixpress/workshop/Main.scala
@@ -8,13 +8,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object Main extends App {
-  println("Hello, world!")
-
   val backend = HttpClientFutureBackend()
   val mapper = JsonMapper.global
 
   val future = Future {
-    println("hello")
+    println("hello world")
   }
 
   Await.result(future, 10.seconds)
