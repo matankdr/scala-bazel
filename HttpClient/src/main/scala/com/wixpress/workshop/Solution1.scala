dc4854380a8c9f4fb63fe91f690a2c5c75f154e4
Matan Keidar <matank@wix.com>
2023-07-23 18:31:22 +0300

    solution

---
+++ HttpClient/src/main/scala/com/wixpress/workshop/Solution1.scala
@@ -0,0 +1,21 @@
+package com.wixpress.workshop
+
+import sttp.client3._
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+object Solution1 extends App with EitherOps {
+  val backend = HttpClientFutureBackend()
+  val encoded = "dmVsbyBpcyBhd2Vzb21lISEhISEK"
+
+  val request = basicRequest.get(uri"http://localhost:8080/base64/$encoded")
+
+  val result = for {
+    response <- request.send(backend)
+    body     <- response.body.toFuture
+  } yield println(body)
+
+  Await.result(result, 10.seconds)
+}
